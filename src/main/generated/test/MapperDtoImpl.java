9c5d2948457cb8d02cc8faed4dffa3b16a4d7dd1
vadim-hleif <vadzim.hleif@gmail.com>
2020-03-04 23:26:32 +0500

    feat(*): clean up extra deps for issue demo

--- src/main/generated/test/MapperDtoImpl.java
+++ src/main/generated/test/MapperDtoImpl.java
@@ -4,11 +4,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-<<<<<<< Updated upstream
-    date = "2020-02-29T17:55:23+0500",
-=======
-    date = "2020-03-04T23:16:07+0500",
->>>>>>> Stashed changes
+    date = "2020-03-04T23:26:06+0500",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.6 (Oracle Corporation)"
 )
 public class MapperDtoImpl implements MapperDto {
